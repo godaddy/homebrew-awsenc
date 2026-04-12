031fb09145f77afdd797be5eca1e436d7d5a83e6
Jay Gowdy <jay@gowdy.me>
2026-04-11 20:39:33 -0700

    Add README, align formula style with homebrew-sshenc

--- Formula/awsenc.rb
+++ Formula/awsenc.rb
@@ -4,15 +4,17 @@ class Awsenc < Formula
   version "0.1.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/jgowdy/awsenc/releases/download/v0.1.0/awsenc-aarch64-apple-darwin.tar.gz"
-      sha256 "a31af545b4759851f490f463578541f16e636705215af100adf7507c851ec089"
-    else
-      url "https://github.com/jgowdy/awsenc/releases/download/v0.1.0/awsenc-x86_64-apple-darwin.tar.gz"
-      sha256 "28768a2e6cbefe6921a5f76ac8c8d5b84769f28a4e7d912f0349cee95643b25d"
-    end
+  on_arm do
+    url "https://github.com/jgowdy/awsenc/releases/download/v0.1.0/awsenc-aarch64-apple-darwin.tar.gz"
+    sha256 "PLACEHOLDER_ARM64_SHA256"
   end
+
+  on_intel do
+    url "https://github.com/jgowdy/awsenc/releases/download/v0.1.0/awsenc-x86_64-apple-darwin.tar.gz"
+    sha256 "PLACEHOLDER_X86_64_SHA256"
+  end
+
+  depends_on :macos
 
   def install
     bin.install "awsenc"
@@ -30,10 +32,10 @@ class Awsenc < Formula
       To add shell integration (export detection + awsenc-use):
 
         # bash: add to ~/.bashrc
-        eval "\$(awsenc shell-init bash)"
+        eval "$(awsenc shell-init bash)"
 
         # zsh: add to ~/.zshrc
-        eval "\$(awsenc shell-init zsh)"
+        eval "$(awsenc shell-init zsh)"
 
         # fish: add to ~/.config/fish/config.fish
         awsenc shell-init fish | source
@@ -41,6 +43,6 @@ class Awsenc < Formula
   end
 
   test do
-    assert_match "awsenc", shell_output("\#{bin}/awsenc --help")
+    assert_match "awsenc", shell_output("#{bin}/awsenc --help")
   end
 end
---
+++ README.md
@@ -0,0 +1,23 @@
+# homebrew-awsenc
+
+Homebrew tap for [awsenc](https://github.com/jgowdy/awsenc) -- hardware-backed AWS credential manager using macOS Secure Enclave.
+
+## Install
+
+```bash
+brew tap jgowdy/awsenc
+brew install awsenc
+```
+
+## Upgrade
+
+```bash
+brew upgrade awsenc
+```
+
+## Uninstall
+
+```bash
+brew uninstall awsenc
+brew untap jgowdy/awsenc
+```
